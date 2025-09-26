964dd8da1d42894911316ce9996a9ad8c4f8d661
Rae <rachaelwilkens@gmail.com>
2025-09-26 16:30:55 +0000

    1.0

---
+++ index.js
@@ -0,0 +1,31 @@
+function contact(event) {
+    event.preventDefault();
+    const loading = document.querySelector('.modal__overlay--loading')
+    const success = document.querySelector('.modal__overlay--success')
+    loading.classList += " modal__overlay--visible" 
+    emailjs
+    .sendForm(
+        'service_3taky7f',
+        'template_hltfhiw',
+        event.target,
+        'kqXVQSSOW_8zEboQV'
+    ).then(() => {
+        loading.classList.remove("modal__overlay--visible")
+        success.classList =+ " modal__overlay--visible"
+    }).catch(() => {
+        loading.classList.remove("modal__overlay--visible")
+        alert(
+            "The email service is temporarily unavailable. Please contact me directly on allmightyrae@gmail.com"
+        );
+    })
+}
+
+let isModalOpen = false;
+function toggleModal() {
+    if (isModalOpen) {
+        isModalOpen = false
+        return document.body.classList.remove("modal--open")
+    }
+    isModalOpen = !isModalOpen;
+    document.body.classList += " modal--open";
+}
